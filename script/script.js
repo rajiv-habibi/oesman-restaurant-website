2b544aca586bfb1e56bd6405aa51844eaaeb2b8d
rajiv <contact.gemy@gmail.com>
2020-06-14 13:09:00 +0700

    oesman

---
+++ script/script.js
@@ -0,0 +1,7 @@
+const header = document.querySelector('header');
+const main = document.querySelector('main');
+header.addEventListener('click', function () {
+    window.scrollTo(
+        0, 785
+    )
+})
